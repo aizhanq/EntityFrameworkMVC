faa2b588854449ee9236c05bbeb2d94ce501725e
aizhanq <aizhankarasartova@gmail.com>
2024-01-30 16:50:45 +0600

    add UsersController, views

---
+++ EntityFrameworkMVC/Data/ApplicationContext.cs
@@ -0,0 +1,17 @@
+﻿using EntityFrameworkMVC.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace EntityFrameworkMVC.Data
+{
+    public class ApplicationContext : DbContext
+    {
+        public DbSet<User> Users { get; set; } = null!;
+        public DbSet<Company> Companies { get; set; } = null!;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=eftestdb;Trusted_Connection=True;");
+        }
+    }
+}
