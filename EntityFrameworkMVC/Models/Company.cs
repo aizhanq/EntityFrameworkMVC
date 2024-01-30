4d7f21157996c6b744aa96bb717cb9ebc828b759
aizhanq <aizhankarasartova@gmail.com>
2024-01-30 16:04:59 +0600

    Add project files.

---
+++ EntityFrameworkMVC/Models/Company.cs
@@ -0,0 +1,9 @@
+﻿namespace EntityFrameworkMVC.Models
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public List<User> Users { get; set; } = new();
+    }
+}
---
+++ EntityFrameworkMVC/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace EntityFrameworkMVC.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
